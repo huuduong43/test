957159a147761a5804c65f1ca23abd02e27ca023
huuduong43 <80561213+huuduong43@users.noreply.github.com>
2023-08-18 09:18:20 +0700

    Add files via upload

---
+++ LoginContainer.tsx
@@ -0,0 +1,143 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Link, RouteComponentProps } from 'react-router-dom';
+import login from '@/api/auth/login';
+import LoginFormContainer from '@/components/auth/LoginFormContainer';
+import { useStoreState } from 'easy-peasy';
+import { Formik, FormikHelpers } from 'formik';
+import { object, string } from 'yup';
+import Field from '@/components/elements/Field';
+import tw from 'twin.macro';
+import Button from '@/components/elements/Button';
+import Reaptcha from 'reaptcha';
+import useFlash from '@/plugins/useFlash';
+
+interface Values {
+    username: string;
+    password: string;
+}
+
+const LoginContainer = ({ history }: RouteComponentProps) => {
+    const ref = useRef<Reaptcha>(null);
+    const [token, setToken] = useState('');
+
+    const { clearFlashes, clearAndAddHttpError } = useFlash();
+    const { enabled: recaptchaEnabled, siteKey } = useStoreState((state) => state.settings.data!.recaptcha);
+
+    useEffect(() => {
+        clearFlashes();
+    }, []);
+
+    const onSubmit = (values: Values, { setSubmitting }: FormikHelpers<Values>) => {
+        clearFlashes();
+
+        // If there is no token in the state yet, request the token and then abort this submit request
+        // since it will be re-submitted when the recaptcha data is returned by the component.
+        if (recaptchaEnabled && !token) {
+            ref.current!.execute().catch((error) => {
+                console.error(error);
+
+                setSubmitting(false);
+                clearAndAddHttpError({ error });
+            });
+
+            return;
+        }
+
+        login({ ...values, recaptchaData: token })
+            .then((response) => {
+                if (response.complete) {
+                    // @ts-expect-error this is valid
+                    window.location = response.intended || '/';
+                    return;
+                }
+
+                history.replace('/auth/login/checkpoint', { token: response.confirmationToken });
+            })
+            .catch((error) => {
+                console.error(error);
+
+                setToken('');
+                if (ref.current) ref.current.reset();
+
+                setSubmitting(false);
+                clearAndAddHttpError({ error });
+            });
+    };
+
+    return (
+        <Formik
+            onSubmit={onSubmit}
+            initialValues={{ username: '', password: '' }}
+            validationSchema={object().shape({
+                username: string().required('A username or email must be provided.'),
+                password: string().required('Please enter your account password.'),
+            })}
+        >
+            {({ isSubmitting, setSubmitting, submitForm }) => (
+                <LoginFormContainer title={'Login to Continue'} css={tw`w-full flex`}>
+                    <Field light type={'text'} label={'Username or Email'} name={'username'} disabled={isSubmitting} />
+                    {/* DisplayPassword Start */}
+                    <div css={tw`mt-6`} style={{display: 'inline-block', width: '100%'}}>
+                        <Field light type={'password'} label={'Password'} name={'password'} disabled={isSubmitting} />
+                        <button type="button" style={{transform: 'translate(-10%, -125%)', float: 'right', border: 'none', backgroundColor: 'transparent'}} onClick={_ => {
+                            let passwordElement = document.querySelector('[name=\'password\']');
+                            if (passwordElement == null) return;
+                            let showIcon = document.getElementById('pwd-show');
+                            if (showIcon == null) return;
+                            let hideIcon = document.getElementById('pwd-hide');
+                            if (hideIcon == null) return;
+                            if(passwordElement.getAttribute('type') === 'password') {
+                                passwordElement.setAttribute('type', 'text');
+                                hideIcon.style.display = 'none';
+                                showIcon.style.display = '';
+                            } else {
+                                passwordElement.setAttribute('type', 'password');
+                                hideIcon.style.display = '';
+                                showIcon.style.display='none';
+                            }
+                        }}>
+                            <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" id="pwd-show" style={{display: 'none', backgroundColor: 'white'}}>
+                                <path d="M12 5C5.63636 5 2 12 2 12C2 12 5.63636 19 12 19C18.3636 19 22 12 22 12C22 12 18.3636 5 12 5Z" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                                <path d="M12 15C13.6569 15 15 13.6569 15 12C15 10.3431 13.6569 9 12 9C10.3431 9 9 10.3431 9 12C9 13.6569 10.3431 15 12 15Z" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                            <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" id="pwd-hide" style={{backgroundColor: 'white'}}>
+                                <path d="M20 14.8335C21.3082 13.3317 22 12 22 12C22 12 18.3636 5 12 5C11.6588 5 11.3254 5.02013 11 5.05822C10.6578 5.09828 10.3244 5.15822 10 5.23552M12 9C12.3506 9 12.6872 9.06015 13 9.17071C13.8524 9.47199 14.528 10.1476 14.8293 11C14.9398 11.3128 15 11.6494 15 12M3 3L21 21M12 15C11.6494 15 11.3128 14.9398 11 14.8293C10.1476 14.528 9.47198 13.8524 9.1707 13C9.11386 12.8392 9.07034 12.6721 9.04147 12.5M4.14701 9C3.83877 9.34451 3.56234 9.68241 3.31864 10C2.45286 11.1282 2 12 2 12C2 12 5.63636 19 12 19C12.3412 19 12.6746 18.9799 13 18.9418" stroke="#000000" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                            </svg>
+                        </button>
+                    </div>
+                    {/* DisplayPassword End */}
+                    <div css={tw`mt-6`}>
+                        <Button type={'submit'} size={'xlarge'} isLoading={isSubmitting} disabled={isSubmitting}>
+                            Login
+                        </Button>
+                    </div>
+                    {recaptchaEnabled && (
+                        <Reaptcha
+                            ref={ref}
+                            size={'invisible'}
+                            sitekey={siteKey || '_invalid_key'}
+                            onVerify={(response) => {
+                                setToken(response);
+                                submitForm();
+                            }}
+                            onExpire={() => {
+                                setSubmitting(false);
+                                setToken('');
+                            }}
+                        />
+                    )}
+                    <div css={tw`mt-6 text-center`}>
+                        <Link
+                            to={'/auth/password'}
+                            css={tw`text-xs text-neutral-500 tracking-wide no-underline uppercase hover:text-neutral-600`}
+                        >
+                            Forgot password?
+                        </Link>
+                    </div>
+                </LoginFormContainer>
+            )}
+        </Formik>
+    );
+};
+
+export default LoginContainer;
